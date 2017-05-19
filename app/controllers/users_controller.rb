7aab88715561106ab363fb4cdd52c7ad7e82c479
Zenaido <luis.hernandez@aggiemail.usu.edu>
2017-05-19 00:29:49 -0600

    updated models and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:user])
+    @user = User.find(params[:id])
   end
   def create
     @user = User.new(user_params)
     if @user.save
-
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
